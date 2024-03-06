4b3dcbeaf4caf8410958b29cfd60d61837aeed96
Bexruz Turobjonov <bexruzturobjonov0955@gmail.com>
2024-03-06 16:58:40 +0500

    injector

---
+++ roles/vault_injector/library/vault_leader_ip_finder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+import subprocess
+import json
+
+def vault_leader_ip_finder(vault_address, vault_token):
+    cmd = ["vault", "operator", "raft", "list-peers", "--address={}".format(vault_address)]
+    env = {"VAULT_TOKEN": vault_token}
+    try:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            return False, stderr.decode()
+        
+        for line in stdout.decode().splitlines():
+            if "leader" in line:
+                return True, line.split()[1].split(':')[0]  # Extract and return IP
+        return False, "Leader not found"
+    except Exception as e:
+        return False, str(e)
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            vault_address=dict(type='str', required=True),
+            vault_token=dict(type='str', required=True),
+        ),
+        supports_check_mode=True
+    )
+
+    vault_address = module.params['vault_address']
+    vault_token = module.params['vault_token']
+    
+    is_success, result = vault_leader_ip_finder(vault_address, vault_token)
+
+    if is_success:
+        module.exit_json(changed=False, leader_ip=result)
+    else:
+        module.fail_json(msg=result)
+
+if __name__ == '__main__':
+    main()
