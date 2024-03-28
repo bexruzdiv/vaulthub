405b2e99b08150eddd0478d6bbb4cf9c5b737dce
Bexruz Turobjonov <bexruzturobjonov0955@gmail.com>
2024-03-28 17:47:59 +0500

    module

--- roles/vault_backup/library/vault_leader_ip_finder.py
+++ roles/vault_backup/library/vault_leader_ip_finder.py
@@ -1,43 +1,84 @@
 #!/usr/bin/python
-
 from ansible.module_utils.basic import AnsibleModule
 import subprocess
-import json
+import os
 
-def vault_leader_ip_finder(vault_address, vault_token):
-    cmd = ["vault", "operator", "raft", "list-peers", "--address={}".format(vault_address)]
-    env = {"VAULT_TOKEN": vault_token}
+def find_leader_ip(vault_addr, vault_token):
     try:
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
-        stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            return False, stderr.decode()
-        
-        for line in stdout.decode().splitlines():
-            if "leader" in line:
-                return True, line.split()[1].split(':')[0]  # Extract and return IP
-        return False, "Leader not found"
-    except Exception as e:
-        return False, str(e)
+        env_vars = os.environ.copy()
+        env_vars["VAULT_ADDR"] = vault_addr
+        env_vars["VAULT_TOKEN"] = vault_token
+
+        output = subprocess.check_output(["vault", "operator", "raft", "list-peers"], universal_newlines=True, env=env_vars)
+        lines = output.strip().split('\n')[2:]  # Skip the header
+        for line in lines:
+            fields = line.split()
+            if len(fields) >= 3 and fields[2] == "leader":
+                # Extract the IP address from the address field
+                leader_ip = fields[1].split(':')[0]
+                return leader_ip
+    except subprocess.CalledProcessError as e:
+        # Handle subprocess error if the command fails
+        return None
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            vault_address=dict(type='str', required=True),
-            vault_token=dict(type='str', required=True),
-        ),
-        supports_check_mode=True
+            vault_addr=dict(required=True, type='str', env_var='VAULT_ADDR'),
+            vault_token=dict(required=True, type='str', no_log=True, env_var='VAULT_TOKEN')
+        )
     )
 
-    vault_address = module.params['vault_address']
+    vault_addr = module.params['vault_addr']
     vault_token = module.params['vault_token']
-    
-    is_success, result = vault_leader_ip_finder(vault_address, vault_token)
+#!/usr/bin/python
+from ansible.module_utils.basic import AnsibleModule
+import subprocess
+import os
 
-    if is_success:
-        module.exit_json(changed=False, leader_ip=result)
+def find_leader_ip(vault_addr, vault_token):
+    try:
+        env_vars = os.environ.copy()
+        env_vars["VAULT_ADDR"] = vault_addr
+        env_vars["VAULT_TOKEN"] = vault_token
+
+        output = subprocess.check_output(["vault", "operator", "raft", "list-peers"], universal_newlines=True, env=env_vars)
+        lines = output.strip().split('\n')[2:]  # Skip the header
+        for line in lines:
+            fields = line.split()
+            if len(fields) >= 3 and fields[2] == "leader":
+                # Extract the IP address from the address field
+                leader_ip = fields[1].split(':')[0]
+                return leader_ip
+    except subprocess.CalledProcessError as e:
+        # Handle subprocess error if the command fails
+        return None
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            vault_addr=dict(required=True, type='str', env_var='VAULT_ADDR'),
+            vault_token=dict(required=True, type='str', no_log=True, env_var='VAULT_TOKEN')
+        )
+    )
+
+    vault_addr = module.params['vault_addr']
+    vault_token = module.params['vault_token']
+
+    leader_ip = find_leader_ip(vault_addr, vault_token)
+    if leader_ip:
+        module.exit_json(changed=False, leader_ip=leader_ip)
     else:
-        module.fail_json(msg=result)
+        module.fail_json(msg="Failed to find leader IP")
+
+if __name__ == '__main__':
+    main()
+
+    leader_ip = find_leader_ip(vault_addr, vault_token)
+    if leader_ip:
+        print(leader_ip)
+    else:
+        module.fail_json(msg="Failed to find leader IP")
 
 if __name__ == '__main__':
     main()
